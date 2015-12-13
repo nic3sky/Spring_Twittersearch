c614c7b6f31679f18fbfa377dbf8f5767fda50a4
nic3sky <nic3sky@gmaill.com>
2015-12-13 14:52:23 +0700

    first step

---
+++ src/main/java/masterSpring/MasterSpringApplication.java
@@ -0,0 +1,12 @@
+package masterSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MasterSpringApplication.class, args);
+    }
+}
